e27045f06846e89628973edc5a2938f429fc6524
darker <61489385@qq.com>
2018-07-12 18:21:37 +0800

    springboot框架搭建

---
+++ src/main/java/com/vip/darker/DarkerApplication.java
@@ -0,0 +1,12 @@
+package com.vip.darker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DarkerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DarkerApplication.class, args);
+    }
+}
