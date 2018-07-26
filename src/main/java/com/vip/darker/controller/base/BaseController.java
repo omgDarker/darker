c4e8b1630f682036e5be8160e34ed83f47626ab3
darker <61489385@qq.com>
2018-07-26 15:41:02 +0800

    [20180726]资源模块CRUD

---
+++ src/main/java/com/vip/darker/controller/base/BaseController.java
@@ -0,0 +1,10 @@
+package com.vip.darker.controller.base;
+
+/**
+ * @Auther: Darker
+ * @Date: 2018/7/26 15:18
+ * @Description: 基础controller
+ */
+public class BaseController {
+
+}
---
+++ src/main/java/com/vip/darker/util/Constant.java
@@ -0,0 +1,11 @@
+package com.vip.darker.util;
+
+/**
+ * @Auther: Darker
+ * @Date: 2018/7/26 15:27
+ * @Description:
+ */
+public class Constant {
+
+    public static final String ADMIN_HOME_REDIRECT = "redirect:/admin/home";
+}
