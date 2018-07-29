f863882dbd3e5b3297d7a2a2af6eee67ac194fbe
darker <61489385@qq.com>
2018-07-29 23:10:38 +0800

    [20180729]_[用户模块]_页面+DB修正

--- src/main/resources/db/blog_user.sql
+++ src/main/resources/db/blog_user.sql
@@ -1,8 +1,15 @@
+DROP TABLE
+IF EXISTS blog_user;
+
 CREATE TABLE blog_user (
 	id INT NOT NULL PRIMARY KEY AUTO_INCREMENT COMMENT '用户ID',
 	name VARCHAR (10) COMMENT '姓名',
+	email VARCHAR(50) COMMENT '邮箱',
+	ip VARCHAR(15) COMMENT 'ip地址',
+	area VARCHAR(255) COMMENT '地区',
 	isDelete INT (1) COMMENT '是否删除(1:是,0:否)',
 	creator VARCHAR (10) COMMENT '创建人',
 	createTime datetime COMMENT '创建时间',
 	updateTime TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间'
 ) COMMENT = '用户管理表';
+COMMIT;
