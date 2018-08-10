97fc86755aac97610820c97ab0a82bb15f3e7e4e
darker <61489385@qq.com>
2018-08-10 18:17:31 +0800

    [20180810]_[首页]_详情展示

--- src/main/java/com/vip/darker/controller/IndexController.java
+++ src/main/java/com/vip/darker/controller/IndexController.java
@@ -1,9 +1,15 @@
 package com.vip.darker.controller;
 
+import com.baomidou.mybatisplus.plugins.Page;
+import com.vip.darker.model.DiaryModel;
+import com.vip.darker.system.locator.SystemServiceLocator;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
 
 /**
  * @Auther: Darker
@@ -22,8 +28,39 @@ public class IndexController {
      * @return
      */
     @RequestMapping(value = "/home", method = RequestMethod.GET)
-    public String index() {
-        return INDEX + "/home";
+    public ModelAndView index() {
+        // 返回页面
+        ModelAndView modelAndView = new ModelAndView(INDEX + "/home");
+        // 日记
+        List<DiaryModel> list = SystemServiceLocator.getDiaryService().selectPage(new Page<>(1, 5)).getRecords();
+        // 处理日记长度
+        for (DiaryModel model : list) {
+            model.setContent(model.getContent().substring(0, model.getContent().length() > 100 ? 100 : model.getContent().length()));
+        }
+
+        modelAndView.addObject("list", list);
+
+        return modelAndView;
+    }
+
+    /**
+     * 功能描述: 文章详情页<阅读原文>
+     *
+     * @param: [id]
+     * @return: org.springframework.web.servlet.ModelAndView
+     * @auther: darker
+     * @date: 2018/8/10 16:49
+     */
+    @RequestMapping(value = "/detail/article/{id}", method = RequestMethod.GET)
+    public ModelAndView detailArticle(@PathVariable(value = "id") Integer id) {
+        // 返回页面
+        ModelAndView modelAndView = new ModelAndView(INDEX + "/detail_article");
+        // 返回数据
+        DiaryModel diaryModel = SystemServiceLocator.getDiaryService().selectById(id);
+
+        modelAndView.addObject("object", diaryModel);
+
+        return modelAndView;
     }
 
     /**
