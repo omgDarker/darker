734e738509d885926cc0f18a8bc6e678a6670ddc
darker <61489385@qq.com>
2018-09-09 14:31:32 +0800

    [20180909]_网站首页提醒信息增加样式

--- src/main/resources/db/blog_visitor.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE blog_visitor (
-	userId INT NOT NULL PRIMARY KEY AUTO_INCREMENT COMMENT '游客ID',
-	userName VARCHAR (255) COMMENT '游客名称',
-	email VARCHAR (255) COMMENT '游客邮箱',
-	phone VARCHAR (255) COMMENT '游客手机号码',
-	record VARCHAR (255) COMMENT '游客留言记录',
-	motto VARCHAR (255) COMMENT '游客座右铭'
-) COMMENT = '游客信息表';
