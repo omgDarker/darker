2dbeff33b56a2e5cada0538703961ab3e456c074
wangbingan <wangbingan@sysware.com.cn>
2019-03-06 15:40:44 +0800

    [20190306]_修正

--- src/main/java/com/vip/darker/base/dpattern/factory/bstract/service/impl/Engineer.java
+++ src/main/java/com/vip/darker/base/dpattern/factory/bstract/service/impl/Engineer.java
@@ -6,11 +6,11 @@ import com.vip.darker.base.dpattern.factory.bstract.service.Character;
 public class Engineer implements Character {
     @Override
     public void testiness() {
-        System.out.println("old is not testiness");
+        System.out.println("engineer is not testiness");
     }
 
     @Override
     public void blandness() {
-        System.out.println("old can be blandness");
+        System.out.println("engineer can be blandness");
     }
 }
